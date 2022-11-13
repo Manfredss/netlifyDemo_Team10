77ca72ce4ef89b214122dade6cafc961013b330e
MartianPig_Marcus <74716511+Manfredss@users.noreply.github.com>
2022-11-13 22:28:25 +0800

    Update xxx.js

--- xxx.js
+++ xxx.js
@@ -1,6 +1,4 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
+function pprint(response) {
 
     // 发送 HTTP 头部 
     // HTTP 状态值: 200 : OK
@@ -9,7 +7,4 @@ http.createServer(function (request, response) {
 
     // 发送响应数据 "Hello World"
     response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+}
