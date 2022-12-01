debd0a3aa86d160c9e0581e89dda781bbd9f3e82
MartianPig_Marcus <74716511+Manfredss@users.noreply.github.com>
2022-12-01 23:27:30 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    <h1 class="green">Welcome!</h1>
-    <!-- <p class="green"></p> -->
     <img id="pic" width="840" height="600" :src="imgs">
 
     <div>
       <button type="primary" style="font-size: 25px;" @click="PreviousImage">Previous Image</button>
       <button type="primary" style="font-size: 25px; left: 507px;" @click="NextImage">Next Image</button>
-      <!-- <button type="primary" style="font-size: 25px;" @click="LoadURL">Load URL</button> -->
     </div>
   </div>
 
@@ -18,29 +15,34 @@
         <input type="radio" id="3" name="Q1" style="left:140px;"><label for="3" style="left:150px;">Colour</label>
       </p>
       <br>
+
       <p>Q2. Is the color used for Aesthetics or Data Visualisation?<br>
         <input type="radio" id="1" name="Q2" style="left: 60px"><label for="1" style="left:70px;">Aesthetics</label>
         <input type="radio" id="2" name="Q2" style="left:100px;"><label for="2" style="left:110px;">Data Visualisation</label>
         <input type="radio" id="3" name="Q2" style="left:140px;"><label for="3" style="left:150px;">Not sure</label>
       </p>
       <br>
+
       <p>Q3. Is a colour mapping legend showed?<br>
         <input type="radio" id="1" name="Q3" style="left: 60px"><label for="1" style="left:70px;">Yes</label>
         <input type="radio" id="2" name="Q3" style="left:100px;"><label for="2" style="left:110px;">No</label>
         <input type="radio" id="3" name="Q3" style="left:140px;"><label for="3" style="left:150px;">Not sure</label>
       </p>
       <br>
+
       <p>Q4. How many colours are used in this image?<br>
         <textarea style="font-size: 25px; text-align: center; resize: none" placeholder="Enter the number" :rows="1"></textarea>
         <input type="radio" id="1" style="left:140px;"><label for="1" style="left:150px;">Not sure</label>
       </p>
       <br>
+
       <p>Q5. Which colour mapping is used, continuous or categorical?<br>
         <input type="radio" id="1" name="Q5" style="left: 60px"><label for="1" style="left:70px;">continuous</label>
         <input type="radio" id="2" name="Q5" style="left:100px;"><label for="2" style="left:110px;">Categorical</label>
         <input type="radio" id="3" name="Q5" style="left:140px;"><label for="3" style="left:150px;">Not sure</label>
       </p>
       <br>
+      
       <p>Q6. From 1-5, how would you rank the difficulty of identifying this image?<br>
         <input type="radio" id="1" name="Q6" style="left: 60px"><label for="1" style="left:70px;">1</label>
         <input type="radio" id="2" name="Q6" style="left:100px;"><label for="2" style="left:110px;">2</label>
@@ -53,7 +55,7 @@
 </template>
 
 <script>
-import urlJson from "/url.json"
+import urlJson from "/url.json";
 export default{
   data(){
     return{
@@ -119,8 +121,26 @@ h1{
   height: 50px;
   left: 90px; 
   top: 15px;
+  border-radius: 6px;
+  font: Arial;
+}button:hover{
+  border-radius: 5px;
+  -webkit-box-shadow: 3px 3px 3px rgba(0,0,0, .29), inset 1px 1px 1px rgba(255,255,255, .44);
+  
+  -webkit-transition: all 0.15s ease;  
+  transition: all 0.15s ease; 
+}
+input[type="radio"]{
+  width: 15px;
+  height: 15px;
 }
 </style>
+
+<!-- -moz-box-shadow: 3px 3px 3px rgba(0,0,0, .29), inset 1px 1px 1px rgba(255,255,255, .44);   -->
+  <!-- box-shadow: 3px 3px 3px rgba(0,0,0, .29), inset 1px 1px 1px rgba(255,255,255, .44);   -->
+  <!-- -moz-transition: all 0.15s ease; 
+  -o-transition: all 0.15s ease;  
+  -ms-transition: all 0.15s ease; -->
 
 <!-- fetch('./url.json')
           .then(function(response) {
